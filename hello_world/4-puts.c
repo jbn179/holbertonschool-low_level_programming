bae7d0eb6f2ddb19b3aee64bcab86e61397af0fc
root <root@Benjamin>
2024-10-21 12:17:52 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
