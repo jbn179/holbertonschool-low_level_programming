fbf49ed5fc00d30d07b225cf77484a88ebf48814
jbn179 <9772@holbertonstudents.com>
2024-10-23 11:32:12 +0200

    Write a function that prints the 9 times table, starting with 0.
    Prototype: void times_table(void);
    Format: see example

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -34,7 +34,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((rs / 10) + '0');
-				_putchar((rs / 10) + '0');
+				_putchar((rs % 10) + '0');
 			}
 		}
 		 _putchar('\n');
