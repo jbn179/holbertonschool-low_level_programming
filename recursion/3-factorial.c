17e941507f99649f9441a456d989a9ed3a75019f
jbn179 <9772@holbertonstudents.com>
2024-11-06 14:46:13 +0100

    projet2

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -10,7 +10,6 @@
 
 int factorial(int n)
 {
-
 	if (n < 0)
 	{
 		return (-1);
@@ -21,6 +20,5 @@ int factorial(int n)
 		return (1);
 	}
 
-	return (1 * factorial(n - 1));
-
+	return (n * factorial(n - 1));
 }
