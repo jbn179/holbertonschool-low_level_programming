b4c93d160676e1b69d38285508870bbe0f5aeab4
jbn179 <9772@holbertonstudents.com>
2024-11-07 14:22:07 +0100

    Write a program that prints the number of arguments passed into it.

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints its name
+ * main - program that prints its name, followed by a new line
  * @argc: number of arguments
  * @argv: array of arguments
  * Description: program that prints its name, followed by a new line
