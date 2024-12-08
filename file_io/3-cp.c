08aa304e819cb481d26cd75d8c9df74578c6bfce
jbn179 <9772@holbertonstudents.com>
2024-12-08 19:46:17 +0100

    file_io

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,65 +1,80 @@
 #include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 void handle_error(const char *message, const char *file, int exit_code);
 void close_file(int fd);
+void copy_file(const char *file_from, const char *file_to);
 
 /**
  * main - program that copies the content of a file to another file.
  * @argc: the number of arguments
  * @argv: the array of arguments
- * Description: program that copies the content of a file to another file.
  * Return: 0 on success, or exit code on failure
  */
-
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, rd, wr;
-	char buffer[1024];
-
 	if (argc != 3)
 		handle_error("Usage: cp file_from file_to\n", NULL, 97);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		handle_error("Error: Can't read from file %s\n", argv[1], 98);
+	copy_file(argv[1], argv[2]);
+	return (0);
+}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+/**
+ * copy_file - copies content of one file to another
+ * @file_from: source file
+ * @file_to: destination file
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to;
+	ssize_t rd, wr;
+	char *buffer = malloc(1024);
+
+	if (!buffer)
+		handle_error("Error: Can't allocate memory\n", NULL, 99);
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		free(buffer);
+		handle_error("Error: Can't read from file %s\n", file_from, 98);
+	}
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
+		free(buffer);
 		close_file(fd_from);
-		handle_error("Error: Can't write to %s\n", argv[2], 99);
+		handle_error("Error: Can't write to %s\n", file_to, 99);
 	}
 
-	while ((rd = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
+	do {
+		rd = read(fd_from, buffer, 1024);
+		if (rd == -1)
+			break;
 		wr = write(fd_to, buffer, rd);
-		if (wr != rd)
-		{
-			close_file(fd_from);
-			close_file(fd_to);
-			handle_error("Error: Can't write to %s\n", argv[2], 99);
-		}
-	}
+		if (wr == -1)
+			break;
+	} while (rd > 0);
 
-	if (rd == -1)
-	{
-		close_file(fd_from);
-		close_file(fd_to);
-		handle_error("Error: Can't read from file %s\n", argv[1], 98);
-	}
-
+	free(buffer);
 	close_file(fd_from);
 	close_file(fd_to);
 
-	return (0);
+	if (rd == -1)
+		handle_error("Error: Can't read from file %s\n", file_from, 98);
+	if (wr == -1)
+		handle_error("Error: Can't write to %s\n", file_to, 99);
 }
 
 /**
  * close_file - closes a file descriptor and handle potential errors
  * @fd: the file descriptor to close
- * Description: closes a file descriptor and handle potential errors
-*/
-
+ */
 void close_file(int fd)
 {
 	if (close(fd) == -1)
@@ -74,9 +89,7 @@ void close_file(int fd)
  * @message: the error message format string
  * @file: the name of the file associated with the error (can be NULL)
  * @exit_code: the exit code to use when terminating the program
- * Description:  prints an error message and exits the program
-*/
-
+ */
 void handle_error(const char *message, const char *file, int exit_code)
 {
 	if (file)
